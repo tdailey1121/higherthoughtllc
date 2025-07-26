bd344fa5294f6d41df762155ec6fe1399c74ecda
tdailey1121 <dailey.t@outlook.com>
2025-07-25 21:45:23 -0400

    Update main.js
    update for mobile nav links

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,7 @@
 function enterLightbulb() {
   window.location.href = "about.html";
 }
+function toggleMenu() {
+  const nav = document.getElementById('navLinks');
+  nav.classList.toggle('show');
+}
