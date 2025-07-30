ef8152755488d36192d3bf117ae7853a6ad01a17
tdailey1121 <dailey.t@outlook.com>
2025-07-29 20:50:12 -0400

    Update main.js
    added ripple overlay

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,22 @@
 function enterLightbulb() {
   const flare = document.querySelector(".flare");
   const ripple = document.querySelector(".ripple-overlay");
+  const gradient = document.getElementById("gradientOverlay");
 
   flare.classList.add("show");
   ripple.classList.add("show");
+  gradient.classList.add("show");
 
   setTimeout(() => {
     document.body.classList.add("fade-out");
-  }, 800); // allow the ripple to peak before fading screen
+  }, 800);
 
   setTimeout(() => {
     window.location.href = "about.html";
-  }, 1600); // adjust as needed for smooth transition
+  }, 1600);
+}
+
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("show");
 }
